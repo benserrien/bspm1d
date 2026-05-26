dd521446de2f067bb0480cbc915a88f63ae14984
Ben Serrien <49861112+benserrien@users.noreply.github.com>
2026-05-26 23:11:50 +0200

    wip, 1st workable version bf_pp

--- R/bspm-bfttest.R
+++ R/bspm-bfttest.R
@@ -20,15 +20,14 @@
 #'
 #' @export
 bspm_bfttest <- function(data, group, dimension, outcome, paired,
-                         nullInterval = c(-0.20, 0.20),
+                         nullInterval = c(-0.2, 0.2),
                          rscale = "medium") {
   # checking input arguments
   check_args()
 
   # point-wise calculations: Bayes-Factor & posterior probability
-  bft <- bspm_bf(data1, data2, nullInterval, rscale)
-  ppt <- bspm_bf2pp(bft)
-
+  bf_pp <- bspm_bfpp(data, group, dimension, outcome, paired,
+                     nullInterval, rscale)
   # Q-value calculation
 
   # identification of supra-threshold clusters
@@ -49,7 +48,12 @@ check_args <- function() {
     is.logical(paired),
     length(paired) == 1,
     length(nullInterval) == 2,
-    rscale %in% c("medium", "wide", "ultrawide")
+    rscale %in% c("medium", "wide", "ultrawide"),
+    length(unique(data[[group]])) == 2
+  )
+  # the data.frame should not contain missing values
+  stopifnot(
+    sum(is.na(data)) == 0
   )
   # at least 3 observations per group per time point (dimension)
   data_check <- data %>%
@@ -58,6 +62,16 @@ check_args <- function() {
   if (nrow(data_check) != 0) {
     stop("Some groups have less than 3 observations on at least 1 element of the 1-dimensional domain.")
   }
+  # for paired data: same number of observations per group/dimension
+  if (paired) {
+    data_check_paired <- data %>%
+      summarise(.by = c(dimension, group), N = n()) %>%
+      summarise(.by = dimension, N2 = n_distinct(N)) %>%
+      filter(N2 != 1)
+    if (nrow(data_check_paired) != 0) {
+      stop("Paired data require the same number of observations at each element of the 1-dimensional domain.")
+    }
+  }
 }
 
 #' bspm_bf
@@ -65,10 +79,13 @@ check_args <- function() {
 #' @description
 #' A short description...
 #'
-bspm_bf <- function(data1, data2, nullInterval, rscale) {
-  bfi <- ttestBF(data1, data2,
-                 nullInterval = nullInterval,
-                 rscale = rscale)
+#' @import BayesFactor
+#' @export
+bspm_bf <- function(data1, data2, ...) {
+  bfi <- BayesFactor::ttestBF(
+    data1, data2,
+    nullInterval = nullInterval, rscale = rscale, paired = paired
+  )
   return(bfi[2] / bfi[1])
 }
 
@@ -84,6 +101,42 @@ bspm_bf2pp <- function(bf) {
   prior.odds <- BayesFactor::newPriorOdds(bf, type = "equal")
   post.odds  <- prior.odds * bf
   return( BayesFactor::as.BFprobability(post.odds) )
+}
+
+#' bspm_bfpp
+#'
+#' @description
+#' Pointwise calculation of the BayesFactor and transformation to posterior probabilities.
+#'
+#' @seealso [bspm_bfttest(), bspm_bf(), bspm_bf2pp()]
+#'
+#' @export
+bspm_bfpp <- function(data, group, dimension, outcome, paired,
+                      nullInterval, rscale) {
+  # make numeric group names
+  data[[group]] <- as.numeric(as.factor(data[[group]]))
+  pw <- data %>%
+    select(all_of(c(dimension, group, outcome))) %>%
+    pivot_wider(values_from = outcome, names_from = group,
+                names_prefix = "Y_", values_fn = list) %>%
+    mutate(
+      bft = map2(Y_1, Y_2, .f = bspm_bf,
+                 paired = paired,
+                 nullInterval = nullInterval,
+                 rscale = rscale),
+      ppt = map(bft, .f = bspm_bf2pp)
+    )
+
+  bf_10 <- sapply(pw$bft, FUN = function(x) as.numeric(as.vector(x)))
+  pp_h1 <- sapply(pw$ppt, FUN = function(x) as.numeric(as.vector(x)))[1,]
+  pp_h0 <- sapply(pw$ppt, FUN = function(x) as.numeric(as.vector(x)))[2,]
+
+  data.frame(
+    dimension = unique(data[[dimension]]),
+    bf_10,
+    pp_h1,
+    pp_h0
+  )
 }
 
 #' bspm_pp2qv
