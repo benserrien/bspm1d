3e02d3a7877d547aef08f7ad906d4f6adababf2c
Ben Serrien <49861112+benserrien@users.noreply.github.com>
2026-06-13 15:53:38 +0200

    moved to subfolder

--- data-raw/cleaning-data-publication.R
+++ data-raw/cleaning-data-publication.R
@@ -1,5 +1,11 @@
 # script to clean the data that were part of the original publication
 
 library(tidyverse)
+library(here)
+
+
+# gait_symmetry
+load(here("data-raw", "data-publication-IntBiomech", "ex_GaitSymmetry.RData"))
+
 
 
