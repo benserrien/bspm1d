fa7f857eca28233e53bdb31bd07f4a15fe8da524
Ben Serrien <49861112+benserrien@users.noreply.github.com>
2026-06-14 14:03:57 +0200

    reformatted datasets

--- data-raw/cleaning-data-publication.R
+++ data-raw/cleaning-data-publication.R
@@ -4,8 +4,70 @@ library(tidyverse)
 library(here)
 
 
-# gait_symmetry
+# -------------------------------------------------------------------------
+# gait_symmetry -----------------------------------------------------------
+
 load(here("data-raw", "data-publication-IntBiomech", "ex_GaitSymmetry.RData"))
+str(knee_angle)
+
+gait_symmetry <- knee_angle %>%
+  mutate(
+    leg = rep(c("right_leg","left_leg"), each = 99),
+    gait_cycle = rep(1:99, 2),
+    .before = V1
+  ) %>%
+  nest(.by = c(gait_cycle, leg)) %>%
+  pivot_wider(values_from = data, names_from = leg)
 
 
+
+# -------------------------------------------------------------------------
+# plantar_arch_angle ------------------------------------------------------
+
+load(here("data-raw", "data-publication-IntBiomech",
+          "ex_PlantarArchAngle.RData"))
+str(Y)
+
+plantar_arch_angle <- Y %>%
+  mutate(
+    condition = rep(c("condition 1","condition 2"), each = 10),
+    person = paste0("SUBJ_", sprintf("%02d", rep(1:10, 2))),
+    .before = V1
+  ) %>%
+  nest(.by = c(person, condition)) %>%
+  pivot_wider(values_from = data, names_from = condition)
+
+
+
+
+# -------------------------------------------------------------------------
+# simulated_two_local_max -------------------------------------------------
+
+load(here("data-raw", "data-publication-IntBiomech",
+          "ex_SimulatedTwoLocalMax.RData"))
+str(Y)
+
+simulated_two_local_max <- Y %>%
+  mutate(
+    group  = rep(c("group 1","group 2"), each = 6),
+    simrep = paste0("SIMREP_", sprintf("%02d", 1:12)),
+    .before = V1
+  ) %>%
+  nest(.by = c(simrep, group))
+
+
+# -------------------------------------------------------------------------
+# save as package data ----------------------------------------------------
+
+usethis::use_data(
+  gait_symmetry, overwrite = TRUE
+)
+
+usethis::use_data(
+  plantar_arch_angle, overwrite = TRUE
+)
+
+usethis::use_data(
+  simulated_two_local_max, overwrite = TRUE
+)
 
